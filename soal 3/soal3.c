f034220b7c05e1e3703258d1bf8a84dae6650713
IsnainiNurul <36990780+IsnainiNurul@users.noreply.github.com>
2019-03-15 21:44:55 +0700

    Create soal3.c

---
+++ soal 3/soal3.c
@@ -0,0 +1,70 @@
+#include <dirent.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+int main()
+{
+    int status;
+    pid_t anak1;
+    int pepiku[4];
+        	pipe(pepiku); 
+        	pipe(pepiku+2);
+    char rest[2000];
+    anak1 = fork();
+    if(anak1 == 0)
+    {
+      execl("/usr/bin/unzip","unzip", "campur2.zip", NULL);
+    }
+	else
+	{
+	    	pid_t anak2;
+        	int p2;
+		while(wait(&status)>0);
+       		anak2 = fork();
+        	if(anak2==0)
+		{
+			close(pepiku[0]);
+          		close(pepiku[2]);
+			close(pepiku[3]);
+          		dup2(pepiku[1], STDOUT_FILENO); 
+          		close(pepiku[1]);
+		char *argv[3] = {"ls", "/home/isnaini/praktikum2/campur2", NULL};
+    execv("/bin/ls", argv);
+}
+		else
+		{
+    			pid_t anak3;
+  	          	anak3 = fork();
+	    		if(anak3==0)
+			{
+	              		close(pepiku[1]);
+	             		dup2(pepiku[0],STDIN_FILENO);
+	              		close(pepiku[0]);
+	              		close(pepiku[2]);
+	              		dup2(pepiku[3],STDOUT_FILENO);
+	              		close(pepiku[3]);
+
+			char *argv[4] = {"grep", "grep",".txt$", NULL};
+    execv("/bin/grep", argv);
+            		}
+			else
+			{
+              			close(pepiku[1]); 
+	             		int cetak = read(pepiku[2],rest,sizeof(rest)); 
+              			close(pepiku[2]);
+              			FILE* file;
+              			file = fopen("daftar.txt","w+");
+             			fprintf(file," Berikut Berupa File berformat .txt :\n%.*s\n",cetak,rest);
+             			printf("Berikut Berupa File berformat .txt :\n%.*s\n",cetak,rest);
+              			fclose(file);
+            	}
+        }
+    }
+}
