6a79cdec0343946c69f8d23c1df12cc1d9b8a75e
IsnainiNurul <36990780+IsnainiNurul@users.noreply.github.com>
2019-03-22 16:54:27 +0700

    Update soal3.c

--- soal 3/soal3.c
+++ soal 3/soal3.c
@@ -14,44 +14,44 @@ int main()
     int status;
     pid_t anak1;
     int pepiku[4];
-        	pipe(pepiku); 
-        	pipe(pepiku+2);
+        	pipe(pepiku); // untuk membuat suatu pipe
+        	pipe(pepiku+2);// untuk menambah pipe 
     char rest[2000];
     anak1 = fork();
-    if(anak1 == 0)
+    if(anak1 == 0)// sebagai child
     {
-      execl("/usr/bin/unzip","unzip", "campur2.zip", NULL);
+      execl("/usr/bin/unzip","unzip", "campur2.zip", NULL);// untuk menunzip
     }
-	else
+	else// sebagai parent
 	{
 	    	pid_t anak2;
         	int p2;
 		while(wait(&status)>0);
        		anak2 = fork();
-        	if(anak2==0)
+        	if(anak2==0)//adalah child
 		{
-			close(pepiku[0]);
+			close(pepiku[0]);//read
           		close(pepiku[2]);
 			close(pepiku[3]);
-          		dup2(pepiku[1], STDOUT_FILENO); 
-          		close(pepiku[1]);
+          		dup2(pepiku[1], STDOUT_FILENO); //untuk menyimpan hasil ls nya  untuk di write
+          		close(pepiku[1]);// setiap dup perlu tanda close untuk ditutup
 		char *argv[3] = {"ls", "/home/isnaini/praktikum2/campur2", NULL};
     execv("/bin/ls", argv);
 }
-		else
+		else//adalah parent yang akan membuat anak
 		{
     			pid_t anak3;
   	          	anak3 = fork();
-	    		if(anak3==0)
+	    		if(anak3==0)// ini adalah child
 			{
 	              		close(pepiku[1]);
-	             		dup2(pepiku[0],STDIN_FILENO);
+	             		dup2(pepiku[0],STDIN_FILENO); //untuk read 
 	              		close(pepiku[0]);
 	              		close(pepiku[2]);
-	              		dup2(pepiku[3],STDOUT_FILENO);
+	              		dup2(pepiku[3],STDOUT_FILENO);//untuk  write (membuat)
 	              		close(pepiku[3]);
 
-			char *argv[4] = {"grep", "grep",".txt$", NULL};
+			char *argv[3] = {"grep",".txt$", NULL};
     execv("/bin/grep", argv);
             		}
 			else
@@ -61,7 +61,7 @@ int main()
               			close(pepiku[2]);
               			FILE* file;
               			file = fopen("daftar.txt","w+");
-             			fprintf(file," Berikut Berupa File berformat .txt :\n%.*s\n",cetak,rest);
+             			fprintf(file," Berikut Berupa File berformat .txt :\n%.*s\n",cetak,rest);// memasukkan hasil ls grep ke file
              			printf("Berikut Berupa File berformat .txt :\n%.*s\n",cetak,rest);
               			fclose(file);
             	}
